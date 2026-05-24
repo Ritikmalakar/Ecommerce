b14da4f3c202898c11781df09cee9d53c1fff7d7
ritikmalakar <ritikmalakar445@gmail.com>
2026-05-24 16:00:59 +0530

    category auth fix

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,509 +1,268 @@
-import User from "../models/userModels.js";
-import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
-import { sendOtpMail } from "../verify/otpVerify.js";
-import Order from "../models/orderModels.js";
+import React, {
+  useEffect,
+  useState
+} from 'react';
 
-// REGISTER
-export async function register(req, res) {
-  try {
+import Layout from '../Layout';
+import Admin from './Admin';
+import { baseUrl1 } from '../../AxiosR';
+import { toast } from 'react-toastify';
+import CategoryForm from '../form/CategoryForm';
 
-    const userData = {
-      ...req.body
+export default function CategoryR() {
+
+  const [category, setCategory] =
+    useState([]);
+
+  const [name, setName] =
+    useState('');
+
+  const [editId, setEditId] =
+    useState(null);
+
+  // TOKEN
+  const token =
+    localStorage.getItem(
+      "token"
+    );
+
+  // Create + Update
+  const submitData =
+    async (e) => {
+
+      e.preventDefault();
+
+      try {
+
+        let data;
+
+        if (editId) {
+
+          ({ data } =
+            await baseUrl1.post(
+              `/update/${editId}`,
+              { name },
+              {
+                headers: {
+                  Authorization:
+                    `Bearer ${token}`
+                }
+              }
+            ));
+
+        } else {
+
+          ({ data } =
+            await baseUrl1.post(
+              "/cate",
+              { name },
+              {
+                headers: {
+                  Authorization:
+                    `Bearer ${token}`
+                }
+              }
+            ));
+        }
+
+        if (data?.success) {
+
+          toast.success(
+            data?.message
+          );
+
+          setName('');
+          setEditId(null);
+          getAllProduct();
+
+        } else {
+
+          toast.error(
+            data?.message
+          );
+        }
+
+      } catch (error) {
+
+        console.log(error);
+        toast.error(
+          "Invalid Data"
+        );
+      }
     };
 
-    if (
-      !userData.name ||
-      !userData.email ||
-      !userData.password ||
-      !userData.address ||
-      !userData.phone
-    ) {
-      return res.status(401).send({
-        success: false,
-        message: "all fields required"
-      });
-    }
+  // Get Categories
+  const getAllProduct =
+    async () => {
 
-    const existUser =
-      await User.findOne({
-        email:
-          userData.email
-      });
+      try {
 
-    if (existUser) {
-      return res.status(400).send({
-        success: false,
-        message:
-          "user already exist"
-      });
-    }
+        const { data } =
+          await baseUrl1.get(
+            "/getAll"
+          );
 
-    userData.password =
-      await bcrypt.hash(
-        userData.password,
-        10
-      );
+        if (data?.success) {
 
-    const user =
-      await User.create(
-        userData
-      );
-
-    user.password =
-      undefined;
-
-    res.status(201).send({
-      success: true,
-      message:
-        "registration successfully",
-      user
-    });
-
-  } catch (err) {
-
-    console.log(err);
-
-    res.status(500).send({
-      success: false,
-      message:
-        "server error",
-      err
-    });
-  }
-}
-
-// LOGIN
-// LOGIN
-export async function login(req, res) {
-
-  try {
-
-    const { email, password } = req.body;
-
-    if (!email || !password) {
-      return res.status(401).send({
-        success: false,
-        message: "all field required"
-      });
-    }
-
-    const user = await User.findOne({ email });
-
-    if (!user) {
-      return res.status(401).send({
-        success: false,
-        message: "user not found"
-      });
-    }
-
-    const isMatch = await bcrypt.compare(
-      password,
-      user.password
-    );
-
-    if (!isMatch) {
-      return res.status(401).send({
-        success: false,
-        message: "Invalid credential"
-      });
-    }
-
-    const token = jwt.sign(
-      {
-        id: user._id,
-        role: user.role
-      },
-      process.env.SECRET_KEY,
-      {
-        expiresIn: "7d"
-      }
-    );
-
-    // REMOVE PASSWORD
-    user.password = undefined;
-
-    // COOKIE FIX FOR HOSTING
-    res.cookie("token", token, {
-      maxAge: 7 * 24 * 60 * 60 * 1000,
-      httpOnly: true,
-      secure: true,
-      sameSite: "None"
-    });
-
-    return res.status(200).send({
-      success: true,
-      message: "login successfully",
-      token,
-      user
-    });
-
-  } catch (err) {
-
-    console.log(err);
-
-    return res.status(500).send({
-      success: false,
-      message: "error",
-      err
-    });
-  }
-}
-// FORGET PASSWORD
-export async function forgetPassword(req, res) {
-
-  try {
-
-    const {
-      email
-    } = req.body;
-
-    if (!email) {
-      return res.status(400).send({
-        success: false,
-        message:
-          "Email is required"
-      });
-    }
-
-    const user =
-      await User.findOne({
-        email
-      });
-
-    if (!user) {
-      return res.status(404).send({
-        success: false,
-        message:
-          "User not found"
-      });
-    }
-
-    const otp =
-      Math.floor(
-        100000 +
-        Math.random() *
-        900000
-      );
-
-    const otpExpiry =
-      new Date(
-        Date.now() +
-        10 * 60 * 1000
-      );
-
-    user.otp = otp;
-    user.otpExpiry =
-      otpExpiry;
-
-    await user.save();
-
-    await sendOtpMail(
-      otp,
-      email
-    );
-
-    return res.status(200).send({
-      success: true,
-      message:
-        "OTP sent successfully"
-    });
-
-  } catch (err) {
-
-    console.log(err);
-
-    return res.status(500).send({
-      success: false,
-      message:
-        "Internal server error"
-    });
-  }
-}
-
-// VERIFY OTP
-export async function verifyOtp(req, res) {
-
-  try {
-
-    const {
-      otp
-    } = req.body;
-
-    const email =
-      decodeURIComponent(
-        req.params.email
-      );
-
-    if (!otp) {
-      return res.status(400).send({
-        success: false,
-        message:
-          "otp is required"
-      });
-    }
-
-    const user =
-      await User.findOne({
-        email
-      });
-
-    if (!user) {
-      return res.status(401).send({
-        success: false,
-        message:
-          "user not found"
-      });
-    }
-
-    if (
-      user.otpExpiry <
-      new Date()
-    ) {
-      return res.status(401).send({
-        success: false,
-        message:
-          "otp is expire"
-      });
-    }
-
-    if (
-      Number(otp) !==
-      Number(user.otp)
-    ) {
-      return res.status(401).send({
-        success: false,
-        message:
-          "invalid otp"
-      });
-    }
-
-    user.otp = null;
-    user.otpExpiry =
-      null;
-
-    await user.save();
-
-    res.status(200).send({
-      success: true,
-      message:
-        "otp verify successfully"
-    });
-
-  } catch (err) {
-
-    console.log(err);
-
-    res.status(500).send({
-      success: false,
-      message:
-        "error"
-    });
-  }
-}
-
-// CHANGE PASSWORD
-export async function chagePass(req, res) {
-
-  try {
-
-    const {
-      newPass,
-      confirmPass
-    } = req.body;
-
-    const email =
-      decodeURIComponent(
-        req.params.email
-      );
-
-    const user =
-      await User.findOne({
-        email
-      });
-
-    if (!user) {
-      return res.status(404).send({
-        success: false,
-        message:
-          "User not found"
-      });
-    }
-
-    if (
-      !newPass ||
-      !confirmPass
-    ) {
-      return res.status(400).send({
-        success: false,
-        message:
-          "All fields required"
-      });
-    }
-
-    if (
-      newPass !==
-      confirmPass
-    ) {
-      return res.status(400).send({
-        success: false,
-        message:
-          "Passwords do not match"
-      });
-    }
-
-    user.password =
-      await bcrypt.hash(
-        newPass,
-        10
-      );
-
-    await user.save();
-
-    return res.status(200).send({
-      success: true,
-      message:
-        "Password changed successfully"
-    });
-
-  } catch (err) {
-
-    console.log(err);
-
-    return res.status(500).send({
-      success: false,
-      message:
-        "Internal server error"
-    });
-  }
-}
-
-// ALL USERS
-export async function allUser(req, res) {
-
-  try {
-
-    const user =
-      await User.find({});
-
-    res.status(200).send({
-      success: true,
-      message:
-        "fetch",
-      user
-    });
-
-  } catch (err) {
-
-    console.log(err);
-
-    res.status(500).send({
-      success: false,
-      message:
-        "error"
-    });
-  }
-}
-
-// UPDATE ADDRESS
-export async function updateAddress(req, res) {
-
-  try {
-
-    const {
-      id,
-      address
-    } = req.body;
-
-    const user =
-      await User.findByIdAndUpdate(
-        id,
-        {
-          address
-        },
-        {
-          new: true
+          setCategory(
+            data.category
+          );
         }
-      );
 
-    res.status(200).send({
-      success: true,
-      message:
-        "Address Updated",
-      user
-    });
+      } catch (error) {
 
-  } catch (error) {
+        console.log(error);
+      }
+    };
 
-    console.log(error);
+  useEffect(() => {
+    getAllProduct();
+  }, []);
 
-    res.status(500).send({
-      success: false,
-      message:
-        "Error in update",
-      error
-    });
-  }
+  // Delete
+  const deleteData =
+    async (id) => {
+
+      try {
+
+        const { data } =
+          await baseUrl1.post(
+            `/delete/${id}`,
+            {},
+            {
+              headers: {
+                Authorization:
+                  `Bearer ${token}`
+              }
+            }
+          );
+
+        if (data?.success) {
+
+          toast.success(
+            data?.message
+          );
+
+          getAllProduct();
+        }
+
+      } catch (error) {
+
+        console.log(error);
+        toast.error(
+          "Invalid Data"
+        );
+      }
+    };
+
+  return (
+    <Layout>
+
+      <div className='container-fluid py-4'>
+
+        <div className='row'>
+
+          <div className='col-md-3 mb-4'>
+            <div className='card shadow border-0 rounded-4'>
+              <div className='card-body'>
+                <Admin />
+              </div>
+            </div>
+          </div>
+
+          <div className='col-md-9'>
+
+            <div className='card shadow border-0 rounded-4'>
+
+              <div className='card-body p-4'>
+
+                <h1 className='fw-bold text-primary mb-4'>
+                  Category
+                </h1>
+
+                <CategoryForm
+                  submitData={submitData}
+                  value={name}
+                  setValue={setName}
+                  buttonText={
+                    editId
+                      ? "Update"
+                      : "Submit"
+                  }
+                />
+
+                <table className='table table-bordered mt-4'>
+
+                  <thead className='table-dark'>
+                    <tr>
+                      <th>S.NO</th>
+                      <th>Name</th>
+                      <th>Action</th>
+                    </tr>
+                  </thead>
+
+                  <tbody>
+
+                    {category?.map(
+                      (item, idx) => (
+
+                        <tr key={item._id}>
+
+                          <td>
+                            {idx + 1}
+                          </td>
+
+                          <td>
+                            {item.name}
+                          </td>
+
+                          <td>
+
+                            <button
+                              className='btn btn-primary btn-sm me-2'
+                              onClick={() => {
+                                setName(
+                                  item.name
+                                );
+                                setEditId(
+                                  item._id
+                                );
+                              }}
+                            >
+                              Edit
+                            </button>
+
+                            <button
+                              className='btn btn-danger btn-sm'
+                              onClick={() =>
+                                deleteData(
+                                  item._id
+                                )
+                              }
+                            >
+                              Delete
+                            </button>
+
+                          </td>
+
+                        </tr>
+                      )
+                    )}
+
+                  </tbody>
+
+                </table>
+
+              </div>
+
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </Layout>
+  );
 }
-
-// ALL ORDERS
-export const allOrders = async (req, res) => {
-  try {
-
-    const orders =
-      await Order.find({})
-        .populate("buyer")
-        .populate("products")
-        .sort({
-          createdAt: -1
-        });
-
-    res.status(200).send({
-      success: true,
-      orders
-    });
-
-  } catch (error) {
-
-    console.log(error);
-
-    res.status(500).send({
-      success: false,
-      message:
-        "Error in Orders"
-    });
-  }
-};
-
-export const deleteOrder =
-  async (req, res) => {
-
-    try {
-
-      const {
-        id
-      } = req.params;
-
-      await Order.findByIdAndDelete(
-        id
-      );
-
-      res.status(200).send({
-        success: true,
-        message:
-          "Order Deleted"
-      });
-
-    } catch (error) {
-
-      console.log(
-        error
-      );
-
-      res.status(500).send({
-        success: false,
-        message:
-          "Delete Error"
-      });
-    }
-  };
--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,27 +1,30 @@
 import jwt from "jsonwebtoken";
 
+import jwt from "jsonwebtoken";
+
 export async function authUser(req, res, next) {
 
   try {
 
-    const token = req.cookies.token;
+    const token =
+      req.cookies.token ||
+      req.headers.authorization?.replace(
+        "Bearer ",
+        ""
+      );
 
     if (!token) {
       return res.status(401).send({
         success: false,
-        message: "Invalid token"
+        message: "No token"
       });
     }
 
-    // verify token
     const decode = jwt.verify(
       token,
       process.env.SECRET_KEY
     );
 
-    console.log(decode);
-
-    // save decoded user
     req.user = decode;
 
     next();
@@ -30,23 +33,29 @@ export async function authUser(req, res, next) {
 
     console.log(err);
 
-    return res.status(500).send({
+    return res.status(401).send({
       success: false,
-      message: "Error"
+      message: "Invalid token"
     });
   }
+
 }
+
 export async function adminUser(req, res, next) {
 
   try {
 
-    if (req.user && req.user.role === "admin") {
+    if (
+      req.user &&
+      req.user.role === "admin"
+    ) {
       next();
     } else {
 
       return res.status(403).send({
         success: false,
-        message: "Access denied. Admin only"
+        message:
+          "Access denied. Admin only"
       });
     }
 
@@ -56,7 +65,8 @@ export async function adminUser(req, res, next) {
 
     return res.status(500).send({
       success: false,
-      message: "Server Error"
+      message:
+        "Server Error"
     });
   }
 }
