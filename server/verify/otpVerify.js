f49a1a6381e09b6b878834712d3bf8be32436348
ritikmalakar <ritikmalakar445@gmail.com>
2026-05-25 21:04:39 +0530

    change

--- server/verify/otpVerify.js
+++ server/verify/otpVerify.js
@@ -1,23 +1,28 @@
 import nodemailer from "nodemailer";
+import dns from "dns";
+
+dns.setDefaultResultOrder("ipv4first");
 
 export const sendOtpMail = async (otp, email) => {
   try {
     const transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
-      family: 4,
+      port: 465,
+      secure: true,
       auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS,
       },
     });
 
+    await transporter.verify();
+    console.log("SMTP Connected");
+
     await transporter.sendMail({
       from: process.env.MAIL_USER,
       to: email,
       subject: "Password Reset OTP",
-      html: `<h1>${otp}</h1>`
+      html: `<h1>${otp}</h1>`,
     });
 
     console.log("Mail sent");
